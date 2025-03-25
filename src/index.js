0eed571665b9f59c84ff3d5d988dc274d0240715
hadas <atiya.hadas@gmail.com>
2025-03-25 10:46:38 +0200

    add execution premession

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@
     console.log(`start download file`);
      return new Promise((resolve, reject) => {
         console.log(`start download provengo`);
-         const file = fs.createWriteStream(dest);
+         let file = fs.createWriteStream(dest);
          https.get(url, (response) => {
              response.pipe(file);
              file.on('finish', () => {
@@ -26,30 +26,30 @@
 
  async function run() {
      try {
-         const command = core.getInput('command');
-         const stringBasicParameters = core.getInput('basic_parameters');
-         const basicParameters = JSON.parse(core.getInput('basic_parameters'));
-         const stringCommandParameters = core.getInput('command_parameters');
-         const commandParameters = JSON.parse(core.getInput('command_parameters'));
+         let command = core.getInput('command');
+         let stringBasicParameters = core.getInput('basic_parameters');
+         let basicParameters = JSON.parse(core.getInput('basic_parameters'));
+         let stringCommandParameters = core.getInput('command_parameters');
+         let commandParameters = JSON.parse(core.getInput('command_parameters'));
 
          console.log(`command: ${command}`);
          console.log(`basicParameters: ${stringBasicParameters}`);
          console.log(`commandParameters: ${stringCommandParameters}`);
 
          // Define the URL to download from (customize this)
-         const downloadProvengo = 'https://downloads.provengo.tech/unix-dist/deb/Provengo-deb.deb';
-         const outputFilePath = "./Provengo-deb.deb";
+         let downloadProvengo = 'https://downloads.provengo.tech/unix-dist/deb/Provengo-deb.deb';
+         let outputFilePath = "./Provengo-deb.deb";
          // Download the software
          await downloadFile(downloadProvengo, outputFilePath);
 
-         //   const softwarePath = path.resolve('./your-software/your-executable-or-script'); // Adjust as needed
+         //   let softwarePath = path.resolve('./your-software/your-executable-or-script'); // Adjust as needed
 
             // Set permissions if needed (for Unix-like systems)
             fs.chmodSync(outputFilePath, '755'); // Make executable
 
 
          // Unzip the downloaded software if it’s a zip file (adjust as needed)
-         const installProvengo = `sudo apt-get install ${outputFilePath}`;
+         let installProvengo = `sudo apt-get install ${outputFilePath}`;
          exec(installProvengo, (installationError) => {
              if (installationError) {
                  core.setFailed(`Installation failed: ${installationError.message}`);
@@ -61,12 +61,12 @@
          });
 
          // Build the command to run your software
-         const params = [
+         let params = [
              // JSON.stringify(basicParameters),     // Basic parameters
              // JSON.stringify(commandParameters)     // Command-specific parameters
          ].join(' ');
 
-         const fullCommand = `provengo --batch-mode create new/pr`;
+         let fullCommand = `provengo --batch-mode create new/pr`;
 
          exec(fullCommand, (executeError, stdout, stderr) => {
              if (executeError) {
@@ -96,17 +96,17 @@
  run();
 
 
-//const core = require('@actions/core');
-//const github = require('@actions/github');
+//let core = require('@actions/core');
+//let github = require('@actions/github');
 //
 //try {
 //  // `who-to-greet` input defined in action metadata file
-//  const nameToGreet = core.getInput('who-to-greet');
+//  let nameToGreet = core.getInput('who-to-greet');
 //  console.log(`Hello ${nameToGreet}!`);
-//  const time = (new Date()).toTimeString();
+//  let time = (new Date()).toTimeString();
 //  core.setOutput("time", time);
 //  // Get the JSON webhook payload for the event that triggered the workflow
-//  const payload = JSON.stringify(github.context.payload, undefined, 2)
+//  let payload = JSON.stringify(github.context.payload, undefined, 2)
 //  console.log(`The event payload: ${payload}`);
 //} catch (error) {
 //  core.setFailed(error.message);
